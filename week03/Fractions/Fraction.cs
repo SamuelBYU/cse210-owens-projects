d688edc2867da11e27623cb49a9126ca4b03621f
Samuel Owens <2nephi4.16@gmail.com>
2026-09-16 18:21:17 -0700

    Just pushing on MacBook so I can pull it on my PC.

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,54 @@
+using System;
+
+
+public class Fraction
+{
+    private int _top;
+
+    private int _bottom;
+
+   public Fraction()//Constructor
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction( int wholeNumber)//Constructor
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)//Constructor
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int SetTop(int top)
+    {
+        return _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public int SetBottom(int bottom)
+    {
+        return _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return "";
+    }
+    public double GetDecimalValue()
+    {
+        return 0;
+    }
+}
+
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction();
+        Fraction f3 = new Fraction();
+
+        
+        Console.WriteLine();
+
+
     }
+    
 }
